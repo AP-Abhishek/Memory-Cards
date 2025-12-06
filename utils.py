9005fe0d36dd4a01b0dbb19d35293e9bc839f2a8
AP-Abhishek <abhishekap12005@gmail.com>
2025-12-06 20:03:21 +0530

    fix: Fixed a bug

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import os, sys
 
 def get_resource_path(path):
-    base_path = os.path.dirname(os.path.abspath(__file__))
-    if hasattr(sys, "_MEIPASS"):
-        return os.path.join(base_path, path)
-    return os.path.join(os.path.abspath("."), path)
+    if getattr(sys, 'frozen', False):
+        base_path = sys._MEIPASS
+    else:
+        base_path = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(base_path, path)
